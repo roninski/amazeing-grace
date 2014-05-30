ad5b567f3a11bafa4df90258f30444b429ac6e61
StahlKB <StahlKB@users.noreply.github.com>
2014-05-30 20:39:47 +1000

    Update Blood.java
    Commented public method

--- src/Blood.java
+++ src/Blood.java
@@ -1,14 +1,23 @@
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-
+/**
+ * Blood signifies the location of a slain enemy.
+ * Blood replaces Enemy on Enemy death
+ * 
+ * @author Yujin, Luke, Steele, Michael 
+ *
+ */
 public class Blood extends Entity{
 
 	public Blood(GameMap map) {
 		super(map, Entity.DRAW_INDEX_BACKGROUND, Entity.TICK_INDEX_BACKGROUND, EntityType.Ground);
-		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Paints this object's image onto the next display canvas to be drawn.
+	 * @param The canvas to paint on to
+	 */
 	@Override
 	public void draw(Graphics g) {
 		// BLOOD PLOX
