6fac8f63a1c6a9ebff4a0f64cffa82d981312c52
StahlKB <StahlKB@users.noreply.github.com>
2014-05-30 20:37:27 +1000

    Update AmmoBox.java
    Commented public methods

--- src/AmmoBox.java
+++ src/AmmoBox.java
@@ -1,14 +1,26 @@
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-
+/**
+ * The ammo box entity. Ammo boxes set player ammo to 100% of max.
+ * 
+ * @author Yujin, Luke, Steele, Michael 
+ *
+ */
 public class AmmoBox extends Entity{
 
+	/**
+	 * See Entity constructor
+	 */
 	public AmmoBox(GameMap map) {
 		super(map, Entity.DRAW_INDEX_ITEM, Entity.TICK_INDEX_BACKGROUND, EntityType.Ground);
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Paints this object's image onto the next display canvas to be drawn.
+	 * @param The canvas to paint on to
+	 */
 	@Override
 	public void draw(Graphics g) {
 		Rectangle r = g.getClipBounds();
